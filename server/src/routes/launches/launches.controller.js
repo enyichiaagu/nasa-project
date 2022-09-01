7236d234334605e2ae6589aabcdfdcaef72632ef
enyichiaagu <enyichiaagu@gmail.com>
2022-09-01 14:35:43 +0100

    Implemented tests using jest and superagent

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,6 +1,8 @@
 const { 
     getAllLaunches, 
-    addNewLaunch
+    addNewLaunch,
+    existsLaunchWithId,
+    abortLaunchById,
  } = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
@@ -12,7 +14,7 @@ function httpAddNewLaunch(req, res) {
 
     if (!launch.mission || !launch.rocket || !launch.target || !launch.launchDate ) {
         return res.status(400).json({
-            error: "missing required Launch property"
+            error: "Missing required launch property"
         })
     }
 
@@ -27,7 +29,21 @@ function httpAddNewLaunch(req, res) {
     return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res) {
+    let launchId = Number(req.params.id);
+
+    if (!existsLaunchWithId(launchId)) {
+        return res.status(404).json({
+            error: "Launch not found",
+        });
+    }
+
+    const aborted = abortLaunchById(launchId);
+    return res.status(200).json(aborted);
+}
+
 module.exports = { 
     httpGetAllLaunches,
     httpAddNewLaunch,
+    httpAbortLaunch,
 }
