856ab1c5f11a373e45ba92c8efdba974f58ff111
enyichiaagu <enyichiaagu@gmail.com>
2022-09-28 10:21:25 +0100

    imported the mongoose mongodb driver

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,19 @@
 const http = require('node:http');
 
+const mongoose = require('mongoose');
+
 const app = require('./app')
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://admin:Mjkql7tzpO4lA7NX@cluster0.rmdcihk.mongodb.net/?retryWrites=true&w=majority';
+
 const server = http.createServer(app)
 
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
 
     server.listen(PORT, () => {
