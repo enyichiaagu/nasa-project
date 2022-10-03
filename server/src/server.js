3c7677765a9996f7ebef39f34654aba040efd135
enyichiaagu <enyichiaagu@gmail.com>
2022-10-03 11:15:38 +0100

    Connected server to Mongo DB

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,24 @@
 const http = require('node:http');
 
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 
 const app = require('./app')
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
-
 const MONGO_URL = process.env.MONGO_URL;
 
-const server = http.createServer(app)
+const server = http.createServer(app);
+
+mongoose.connection.once('open', () => {
+    console.log('Mongo DB Connection Successful');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+});
 
 async function startServer() {
     await mongoose.connect(MONGO_URL);
