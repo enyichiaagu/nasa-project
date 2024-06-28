78dc7be0a91e1d8fd4bc37c8c502b8af402cb185
enyichiaagu <enyichiaagu@gmail.com>
2024-06-28 15:08:13 +0100

    starter files

---
+++ server/src/server.js
@@ -0,0 +1,11 @@
+import http from 'node:http';
+import app from './app.js';
+import { loadPlanetsData } from './models/planets.model.js';
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+await loadPlanetsData();
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
