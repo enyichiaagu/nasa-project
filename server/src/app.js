9f4f0ecaa75adee07666ab4ede5b06270938ac9d
enyichiaagu <enyichiaagu@gmail.com>
2024-06-29 12:39:30 +0100

    added react build to server

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,9 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import express from 'express';
 import cors from 'cors';
+
 import planetsRouter from './routes/planets/planets.router.js';
 
 const app = express();
@@ -9,7 +13,16 @@ app.use(
 		origin: 'http://localhost:3000',
 	})
 );
+
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+// app.get('/', (req, res) => {
+// 	res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+// });
 
 export default app;
