d064c965ab5cc910c583d8e3daf054637ce29bc5
enyichiaagu <enyichiaagu@gmail.com>
2024-07-08 13:56:15 +0100

    added new server features

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,9 @@
-import { getAllLaunches, addNewLaunch } from '../../models/launches.model.js';
+import {
+	getAllLaunches,
+	addNewLaunch,
+	existsLaunchWithId,
+	abortLaunchById,
+} from '../../models/launches.model.js';
 
 function httpGetAllLaunches(req, res) {
 	return res.status(200).json(Array.from(getAllLaunches()));
@@ -7,10 +12,35 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
 	const launch = req.body;
 
+	if (
+		!launch.mission ||
+		!launch.rocket ||
+		!launch.launchDate ||
+		!launch.target
+	) {
+		return res
+			.status(400)
+			.json({ error: 'Missing required launch property' });
+	}
+
 	launch.launchDate = new Date(launch.launchDate);
+	if (isNaN(launch.launchDate)) {
+		return res.status(400).json({ error: 'Invalid launch date' });
+	}
 
 	addNewLaunch(launch);
 	return res.status(201).json(launch);
 }
 
-export { httpGetAllLaunches, httpAddNewLaunch };
+function httpAbortLaunch(req, res) {
+	const launchId = Number(req.params.id);
+
+	if (!existsLaunchWithId(launchId)) {
+		return res.status(404).json({ error: 'Launch not found' });
+	}
+
+	const aborted = abortLaunchById(launchId);
+	return res.status(200).json(aborted);
+}
+
+export { httpGetAllLaunches, httpAddNewLaunch, httpAbortLaunch };
