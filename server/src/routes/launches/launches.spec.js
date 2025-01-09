4cf8d5ab136c16fc598e9a432f14aa20ff8ad1a2
enyichiaagu <enyichiaagu@gmail.com>
2025-01-09 18:10:34 +0100

    Load Planets data in test

--- server/src/routes/launches/launches.spec.js
+++ server/src/routes/launches/launches.spec.js
@@ -1,10 +1,12 @@
 import request from 'supertest';
 import app from '../../app.js';
 import { mongoConnect, mongoDisconnect } from '../../services/mongo.js';
+import { loadPlanetsData } from '../../models/planets.model.js';
 
 describe('Launches API', () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
